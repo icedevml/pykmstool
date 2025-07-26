a54b805bd8f8aaf05ae8039bbf477559bad74343
Michał Leszczyński <ml@icedev.pl>
2025-07-26 16:46:31 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-gcloud auth application-default login
+if ! test -f /root/.config/gcloud/application_default_credentials.json; then
+  gcloud auth application-default login --no-launch-browser
+fi
+
 python3 pykmstool.py "$@"
