cdd2a1f34d34af007f92ab36d1a2f5062ab8f0a7
Michał Leszczyński <ml@icedev.pl>
2025-07-27 16:00:29 +0200

    Implement ECDSA and EdDSA keys support, fix bug in entrypoint.sh, update README.md (#14)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-if ! test -f /root/.config/gcloud/application_default_credentials.json; then
-  gcloud auth application-default login --no-launch-browser
-fi
-
 if [ "$1" = "docker-revoke-credentials" ];
 then
   gcloud auth application-default revoke
+  rm -rf /root/.config/gcloud/application_default_credentials.json
   exit 0
+fi
+
+if ! test -f /root/.config/gcloud/application_default_credentials.json; then
+  gcloud auth application-default login --no-launch-browser
 fi
 
 /app/.venv/bin/python3 pykmstool.py "$@"
--- kms_sign_csr.py
+++ kms_sign_csr.py
@@ -1,19 +1,19 @@
 from cryptography import x509
-from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives._serialization import Encoding, PublicFormat
 from cryptography.x509 import load_pem_x509_csr
 from google.cloud import kms
 
-from kms_priv_key import GoogleCloudKMSRSAPrivateKey
+from kms_priv_key import create_pyca_private_key
 
 
 def kms_get_public_key(*, client: kms.KeyManagementServiceClient, key_version_name: str) -> str:
-    signer_priv_key = GoogleCloudKMSRSAPrivateKey(client, key_version_name)
+    signer_priv_key = create_pyca_private_key(client, key_version_name)
     return signer_priv_key.public_key().public_bytes(encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo).decode('ascii')
 
 
-def kms_sign_csr(*, client: kms.KeyManagementServiceClient, key_version_name: str, rfc4514_name: str, hash_func: str = "sha256") -> str:
-    signer_priv_key = GoogleCloudKMSRSAPrivateKey(client, key_version_name)
+def kms_sign_csr(*, client: kms.KeyManagementServiceClient, key_version_name: str, rfc4514_name: str) -> str:
+    signer_priv_key = create_pyca_private_key(client, key_version_name)
 
     name = x509.Name.from_rfc4514_string(rfc4514_name)
 
@@ -22,23 +22,13 @@ def kms_sign_csr(*, client: kms.KeyManagementServiceClient, key_version_name: st
         .subject_name(name)
     )
 
-    p_hash_func = hash_func.lower()
-
-    if p_hash_func == "sha256":
-        hash_obj = hashes.SHA256()
-    elif p_hash_func == "sha384":
-        hash_obj = hashes.SHA384()
-    elif p_hash_func == "sha512":
-        hash_obj = hashes.SHA512()
-    else:
-        raise RuntimeError("Unsupported hash function: " + p_hash_func)
-
-    cert = builder.sign(signer_priv_key, hash_obj)
+    cert = builder.sign(signer_priv_key, signer_priv_key.hash_algorithm())
     return cert.public_bytes(serialization.Encoding.PEM).decode('ascii')
 
 
 def kms_verify_csr(*, client: kms.KeyManagementServiceClient, key_version_name: str, csr_pem: str, expected_rfc4514_name: str):
-    signer_priv_key = GoogleCloudKMSRSAPrivateKey(client, key_version_name)
+    signer_priv_key = create_pyca_private_key(client, key_version_name)
+
     csr = load_pem_x509_csr(csr_pem.encode('ascii'))
 
     if csr.subject != x509.Name.from_rfc4514_string(expected_rfc4514_name):
